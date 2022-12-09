f979c5a8b378d6b8aeeceb72a5599f73138d1457
masa-panoply <masa@panoply.io>
2022-12-09 14:26:00 +0100

    Added date range!

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 habits = ["Test habit", "Test habit 2"]
 
 
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+        return dates
+
+    return {"date_range": date_range}
+
+
 @app.route("/")
 def index():
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
+    date_str = request.args.get("date")
+    selected_date = datetime.date.fromisoformat(date_str) if date_str else datetime.date.today()
+    return render_template(
+        "index.html",
+        habits=habits,
+        title="Habit Tracker - Home",
+        selected_date=selected_date
+    )
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -14,4 +31,8 @@ def add_habit():
     if request.method == "POST":
         habit = request.form.get("habit")
         habits.append(habit)
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+    return render_template(
+        "add_habit.html",
+        title="Habit Tracker - Add Habit",
+        selected_date=datetime.date.today()
+    )
